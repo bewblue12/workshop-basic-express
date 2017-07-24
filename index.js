5e61daaf82ab58c1a58d01091af974c58e7879fe
Husky <Nadchanon@thinknet.co.th>
2017-07-24 21:20:55 +0700

    เพิ่ม body-parser + cookie-parser

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
