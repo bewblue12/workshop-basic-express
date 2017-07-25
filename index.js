03961fd44f97770823f002942dbdadd3bba69fa2
Husky <Nadchanon@thinknet.co.th>
2017-07-25 09:55:46 +0700

    ตัวอย่าง error-handling

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
